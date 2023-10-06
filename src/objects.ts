1ae152dc94bda0e3c43d395f262dbaa149514bd7
Isaiah <isaiah.grace@maine.edu>
2023-10-05 23:24:56 -0400

    finish toShortForm

--- src/objects.ts
+++ src/objects.ts
@@ -70,7 +70,8 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    const newString = `${question.id}: ${question.name.slice(0, 10)}`;
+    return newString;
 }
 
 /**
