1d7e77dcd7a5bf6b4880fe69f10efceaf46b22cb
Isaiah G <isaiah.grace@maine.edu>
2023-11-05 21:46:04 -0500

    complete sumPublishedPoints

--- src/nested.ts
+++ src/nested.ts
@@ -92,7 +92,16 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    const publishedPoints = questions.filter(
+        (question: Question): boolean => question.published
+    );
+
+    const publishedSum = publishedPoints.reduce(
+        (sum: number, question: Question) => sum + question.points,
+        0 // init value
+    );
+
+    return publishedSum;
 }
 
 /***
