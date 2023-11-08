67c2d21df8b8270fc4bb7acbc561497451f504a7
Isaiah G <isaiah.grace@maine.edu>
2023-11-08 14:24:30 -0500

    complete isSameType

--- src/nested.ts
+++ src/nested.ts
@@ -181,7 +181,21 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    return false;
+    if (questions.length === 0) {
+        return true;
+    }
+
+    const firstType = questions[0].type;
+
+    function isSameType(question: Question, type: string): boolean {
+        return String(question.type) === type;
+    }
+
+    const isAllSameType: boolean = questions.every(
+        (question: Question): boolean => isSameType(question, firstType)
+    );
+
+    return isAllSameType;
 }
 
 /***
